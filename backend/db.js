97c84a5a6e1c1c01d5fd5985e3930ee375c85f64
bader <babbaoui01@gmail.com>
2026-01-20 13:07:09 +0100

    backend partially done

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,12 @@
-const { Pool } = require("pg");
+import { Pool } from "pg";
 
 const pool = new Pool({
     user: "postgres",
     host: "localhost",
     database: "postgres",
-    password: "[PASSWORD]",
+    password: "postgres",
     port: 5432,
 });
 
-module.exports = pool;
+export default pool;
 
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,63 @@
-import express from "express";
+import express from 'express'
+import pool from './db.js'
 
-const app = express();
+const port = 3000
+const router = express.Router()
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
+const app = express()
+app.use(express.json())
+app.use('/api', router)
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World!')
+})
+
+app.post('/setup', (req, res) => {
+  const { name, location } = req.body
+  res.status(200).send(`Hello ${name} from ${location}`)
+})
+
+app.get('/setup', async (req, res) => {
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL
+      )
+    `)
+    res.status(200).send('Table created successfully')
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'Internal server error' })
+  }
+})
+
+router.get('/users', async (req, res) => {
+  try {
+    const users = await pool.query('SELECT * FROM users')
+    res.json(users.rows)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'Internal server error' })
+  }
+})
+
+router.post('/users', async (req, res) => {
+  try {
+    const { name, email, password } = req.body
+    const result = await pool.query(
+      'INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *',
+      [name, email, password]
+    )
+    res.status(200).send(result.rows[0])
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'Internal server error' })
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`)
+})
